47fcdcd808b9d54db12f8d6e5becf153f4d6dfc9
Muhammad Jazzel Mehmood <jazzelmehmood6@gmail.com>
2021-07-18 06:35:45 -0700

    Added auth and user routes

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const connectDB = require("./config/db")
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+// Connect Database
+connectDB();
+
+// Init middlewares
+app.use(express.json({extended: false}))
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"))
+app.use("/api/auth", require("./routes/api/auth"))
+app.use("/api/posts", require("./routes/api/posts"))
+app.use("/api/profile", require("./routes/api/profile"))
+
+app.get("/", (req, res) => res.send("API running...."))
 
 app.listen(PORT, () => console.log(`Listening at port: ${PORT}`));
